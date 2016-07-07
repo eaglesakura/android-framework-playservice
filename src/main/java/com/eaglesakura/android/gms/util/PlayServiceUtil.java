fc4dcbc01dce81eb42e058de539a3bf47d4b2e26
eaglesakura <eagle.sakura@gmail.com>
2016-07-07 18:04:22 +0900

    SignIn時の挙動をrevokeではなくsignOutに変更

--- src/main/java/com/eaglesakura/android/gms/util/PlayServiceUtil.java
+++ src/main/java/com/eaglesakura/android/gms/util/PlayServiceUtil.java
@@ -1,6 +1,10 @@
 package com.eaglesakura.android.gms.util;
 
+import com.google.android.gms.auth.GoogleAuthException;
+import com.google.android.gms.auth.GoogleAuthUtil;
+import com.google.android.gms.auth.UserRecoverableAuthException;
 import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.common.Scopes;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.PendingResult;
 import com.google.android.gms.common.api.Result;
@@ -15,9 +19,12 @@ import com.eaglesakura.lambda.CallbackUtils;
 import com.eaglesakura.lambda.CancelCallback;
 import com.eaglesakura.thread.Holder;
 
+import android.accounts.Account;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 import java.io.IOException;
 
@@ -29,7 +36,7 @@ public class PlayServiceUtil {
     /**
      * ログインを行うためのIntentを発行する。
      *
-     * 既にログイン済みの場合、アクセスを一旦revokeして再度ログインを促すようにする
+     * 既にログイン済みの場合、アクセスを一旦signOutして再度ログインを促すようにする
      *
      * @param builder        ログイン対象のAPI
      * @param cancelCallback キャンセルチェック
@@ -41,10 +48,30 @@ public class PlayServiceUtil {
                 PlayServiceConnection connection = PlayServiceConnection.newInstance(builder, cancelCallback)
         ) {
             GoogleApiClient client = connection.getClientIfSuccess();
-            PlayServiceUtil.await(Auth.GoogleSignInApi.revokeAccess(client), cancelCallback);
-
+//            await(Auth.GoogleSignInApi.revokeAccess(client), cancelCallback);
+            await(Auth.GoogleSignInApi.signOut(client), cancelCallback);
             return connection.newSignInIntent();
         } catch (IOException e) {
+            throw new PlayServiceException(e);
+        }
+    }
+
+    /**
+     * OAuth2アクセストークンを取得する
+     *
+     * @param email       UserEmail
+     * @param scope       アクセススコープ
+     * @param extraScopes 2個以上のアクセススコープを指定する場合
+     */
+    @Nullable
+    public static String getAccessToken(@NonNull Context context, @NonNull String email, @NonNull String scope, String... extraScopes) throws PlayServiceException {
+        try {
+            return GoogleAuthUtil.getToken(
+                    context,
+                    new Account(email, "com.google"),
+                    "oauth2:" + scope
+            );
+        } catch (Exception e) {
             throw new PlayServiceException(e);
         }
     }
